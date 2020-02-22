766b08fe5903d5ff9d73860c8f708620fd768e9d
Josh Davidson <joshua.davidson@accenture.com>
2020-02-22 16:44:49 -0700

    POC to download, unzip & upload SQL in memory.  Currently exceeds heap size

--- src/cardImporter/downloader.ts
+++ src/cardImporter/downloader.ts
@@ -1,11 +1,37 @@
-const axios = require('axios').default;
-const fs = require('fs');
+import * as AdmZip from 'adm-zip';
+import * as request from 'request';
+import {Database} from '../utils/db';
 
-export const downloadCardSql = async (): Promise<void> => {
-	const zippedSql = await axios({
-		method: 'get',
-		url: 'https://www.mtgjson.com/files/AllPrintings.sql.zip',
-		responseType: 'stream'
+const url = 'https://www.mtgjson.com/files/AllPrintings.sql.zip';
+let sql = '';
+
+const massageSql = (sql: string): string => sql
+	.split('\n')
+	.splice(190)
+	.join('\n')
+	.replace(/INSERT INTO/gi, 'INSERT IGNORE INTO');
+
+(async() => {
+	const db = new Database({
+		host     : 'localhost',
+		user     : 'root',
+		password : 'new_password',
+		database : 'foundry_db',
+		multipleStatements: true,
 	});
-	await zippedSql.data.pipe(fs.createWriteStream('AllPrintings.sqlite.zip'));
-};
+
+	request.get({url, encoding: null}, async (err, res, body) => {
+		var zip = new AdmZip(body);
+		var zipEntries = zip.getEntries();
+		console.log(zipEntries.length);
+
+		zipEntries.forEach((entry) => {
+			sql += zip.readAsText(entry);
+		});
+
+		const cleanSQL = massageSql(sql);
+
+		await db.query(cleanSQL);
+	});
+
+})();
